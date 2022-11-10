2c5d4170a3c85ca09f791e8884dccf4c509cea7a
seonwoo0808 <seonwoo0808@naver.com>
2022-11-10 18:15:40 +0900

    add distance calc

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from pickle import dumps, loads
 
 dotenv.load_dotenv()
 
-
+# Hyperparameters
 
 SERVO_MAX_DUTY = 12
 SERVO_MIN_DUTY = 3 
@@ -23,8 +23,28 @@ SERVO_PIN = 13
 
 BUTTON_PIN = 16
 
+TRIG = 17
+ECHO = 4
+A=5
+B=6
+C=12
+D=16
+E=26
+F=20
+G=21
+
+d1=25
+d2=22
+d3=24
+d4=23
+
+# Global variables
+
 captured_arr = np.empty((480, 640, 3), dtype=np.uint8)
 resized_arr = np.empty((480, 640,), dtype=np.uint8)
+
+distance = 0
+
 # Set up the thread
 def camera_thread():
     with picamera.PiCamera() as camera:
@@ -38,6 +58,21 @@ def camera_thread():
             except KeyboardInterrupt:
                 break
 
+def sonic():
+   sonic_timer_start = time.time()
+   
+   pi.write(TRIG, False)
+   time.sleep(0.01)
+   pi.write(TRIG, True)
+   time.sleep(0.00001)
+   pi.write(TRIG, False)
+   while pi.read(ECHO)==0:
+      sonic_timer_start = time.time()
+   while pi.read(ECHO)==1:
+      sonic_timer_stop = time.time()
+   sonic_time_elapsed = sonic_timer_start - sonic_timer_stop
+   distance = int(sonic_time_elapsed * 17160.0)
+
 async def connect():
 
     # 웹 소켓에 접속을 합니다.
@@ -48,7 +83,7 @@ async def connect():
         if data == "Web Server Hello":
             
             while True:
-                coord = np.array([degree], dtype=np.float32)
+                coord = np.array([degree, distance], dtype=np.float32)
                 await websocket.send(dumps(coord))
                 data = await websocket.recv()
                 if data == "OK":
@@ -72,6 +107,10 @@ pi.set_mode(BUTTON_PIN, pigpio.INPUT)
 pi.set_pull_up_down(BUTTON_PIN, pigpio.PUD_DOWN)
 
 
+# Ultrasonic Initialization
+pi.set_mode(TRIG, pigpio.OUTPUT)
+pi.set_mode(ECHO, pigpio.INPUT)
+
 
 def analog_read(channel):
     r = spi.xfer2([1, (8 + channel) << 4, 0])
@@ -86,20 +125,25 @@ cam_thread_obj = threading.Thread(target=camera_thread, args=(), daemon=True)
 cam_thread_obj.start()
 asyncio.get_event_loop().run_until_complete(connect())
 
+top = {"degree": None, "distance": None}
+bottom = {"degree": None, "distance": None}
+
 while True:
     try:
         analog_value = analog_read(0)*1.9 + 500
         if abs(analog_value - prev_value) > 50:
             prev_value = analog_value
-            print(analog_value)
             pi.set_servo_pulsewidth(SERVO_PIN, analog_value)
             degree = int((analog_value -500)*0.09)
         button_read = pi.read(BUTTON_PIN)
         if button_pushed == False and button_read == 1:
             button_pushed = True
-            print("Button Pushed")
-            cv2.imwrite('image.jpg', output)
-            print("Image Saved")
+            if top["degree"] == None:
+                top["degree"] = degree
+                top["distance"] = distance
+            elif bottom["degree"] == None:
+                bottom["degree"] = degree
+                bottom["distance"] = distance
         elif button_pushed == True and button_read == 0:
             button_pushed = False
         time.sleep(0.01)
